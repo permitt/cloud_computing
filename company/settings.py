8b1425b00419dad1399e2b42742e45c141a6b03c
Mitar Perovic <perovicmitar@uns.ac.rs>
2021-12-04 21:04:35 +0100

    added deps

--- company/settings.py
+++ company/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cloud_computing',
         'USER': 'postgres',
-        'PASSWORD': 'Password1!',
+        'PASSWORD': 'defence1',
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- employee/views.py
+++ employee/views.py
@@ -3,21 +3,30 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from employee.models import EmployeeModel, Counter
 from employee.serializer import EmployeeSerializer
+from rest_framework.decorators import api_view
+from django.http import HttpResponse, JsonResponse
 
-
+@api_view(('GET',))
 def get_all(request: Request) -> Response:
     employees: QuerySet[EmployeeModel] = EmployeeModel.objects.all()
     serializer = EmployeeSerializer(employees, many=True)
-    return Response(serializer.data, status=200)
+    return JsonResponse(serializer.data, status=200, safe=False)
 
+@api_view(('GET',))
 def get_employee(request: Request, id: int) -> Response:
     employee: EmployeeModel = EmployeeModel.objects.get(id=id)
     serializer = EmployeeSerializer(employee)
-    return Response(serializer.data, safe=False, status=200)
+    return JsonResponse(serializer.data, safe=False, status=200)
 
+
+@api_view(('GET',))
 def count(request: Request) -> Response:
-    counter: Counter = Counter.objects.get(id=1)
+    try:
+        counter: Counter = Counter.objects.get(id=1)
+    except:
+        counter: Counter = Counter(id=1)
+
     counter.count += 1
     counter.save()
 
-    return Response(counter.count, status=200)
+    return JsonResponse(counter.count, status=200, safe=False)
